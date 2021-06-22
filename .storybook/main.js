0b33798f423104eee55ffe0fe9717b62fa861fae
keth-dev <77727801+keth-dev@users.noreply.github.com>
2021-06-22 23:16:18 +0200

    add storybook via cli

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
-  stories: ['../projects/my-ui-lib/**/*.stories.mdx', '../projects/my-ui-lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss']
-};
+  "stories": [
+    "../projects/my-ui-lib/**/*.stories.mdx",
+    "../projects/my-ui-lib/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,15 @@
-import '!style-loader!css-loader!sass-loader!../projects/my-ui-lib/src/lib/styles.scss';
-import { setCompodocJson } from '@storybook/addon-docs/angular';
-import docJson from '../documentation.json';
-
+import '!style-loader!css-loader!sass-loader!./../projects/my-ui-lib/src/lib/styles.scss';
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
+
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' }
-};
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
